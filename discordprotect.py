64db1b020c86f7708ab9bf4c2d2d06ef82f57fbb
dangbee <26707967+dangbee@users.noreply.github.com>
2018-02-25 20:14:09 +1030

    Cleanup
    As suggested.

--- discordprotect.py
+++ discordprotect.py
@@ -3,6 +3,7 @@ import random
 import string
 from shutil import copy2
 
+
 def id_generator(size=7, chars=string.ascii_uppercase + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
@@ -22,37 +23,38 @@ user_data_path = '''var userDataPath = _path2.default.join(userDataRoot, 'discor
 
 
 def backup_file(file):
-
     # Get file info to use to reconstruct the new path.
     split = os.path.splitext(file)
     file_name_inc_path = split[0]
     file_ext = split[1]
-    
-    new_file = "{}{}".format(file_name_inc_path + '_backup', file_ext)
+
+    new_file = file_name_inc_path + '_backup' + file_ext
 
     # Verify it is copied
     new_file = copy2(file, new_file)
     return os.path.exists(new_file)
 
+
 def rename(folder_path):
     sec = folder_path.replace(insecure_folder, secure_folder)
     os.rename(folder_path, sec)
-    
-    
-def patch_asar(in_file):
 
+
+def patch_asar(in_file):
     if not backup_file(in_file):
         return False
 
     with open(in_file, 'rb') as file:
         data = file.read()
 
-    data = data.replace(user_data_path.encode('ansi'), user_data_path.replace(insecure_folder, secure_folder).encode('ansi'))
-    
+    data = data.replace(user_data_path.encode('ansi'),
+                        user_data_path.replace(insecure_folder, secure_folder).encode('ansi'))
+
     with open(in_file, 'wb') as file:
         file.write(data)
 
     return True
+
 
 if not patch_asar(bootstrapper_file):
     print('Error patching boostrapper!')
@@ -66,6 +68,6 @@ try:
     rename("{}/{}".format(roaming, insecure_folder))
     print('Discord is now secure. The new folder for discord is', secure_folder)
 except PermissionError:
-    print('Could not rename folder "{}" to "{}" in "{}".\nPlease manually complete this step.'.format(insecure_folder, secure_folder, roaming))
-
-
+    print('Could not rename folder "{}" to "{}" in "{}".\nPlease manually complete this step.'.format(insecure_folder,
+                                                                                                      secure_folder,
+                                                                                                      roaming))
